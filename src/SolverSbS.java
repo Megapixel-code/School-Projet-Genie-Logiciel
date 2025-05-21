dc442bd9e5dd4cfadf302d01d6965c8488d2c1a4
Megapixel-code <chainemegapixel@gmail.com>
2025-05-21 09:14:58 +0200

    creating branch

---
+++ src/SolverSbS.java
@@ -0,0 +1,15 @@
+public class SolverSbS {
+    /*
+     * solver_type =
+     * 0 = dfs 
+     */ 
+    int solver_type;
+    Node last_node;
+    Node start_node;
+    Node end_node;
+
+    
+    public SolverSbS(){
+
+    }
+}
