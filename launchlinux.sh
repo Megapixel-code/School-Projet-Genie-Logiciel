49a94b5b7894200f72186668315fa645ba213a65
Ivan <77630002+Megapixel-code@users.noreply.github.com>
2025-05-23 16:56:10 +0200

    Update launchlinux.sh

--- launchlinux.sh
+++ launchlinux.sh
@@ -1,4 +1,6 @@
-javac --module-path "./School-Projet-Genie-Logiciel/javafx-sdk-17.0.15/lib" --add-modules javafx.controls,javafx.fxml -d bin src/*.java
+#!/usr/bin/env bash
+
+javac --module-path "./linux-javafx-sdk-17.0.15/lib" --add-modules javafx.controls,javafx.fxml -d bin src/*.java
 if [ $? -ne 0 ]; then
   echo "Compilation failed"
   exit 1
@@ -6,4 +8,4 @@ fi
 
 cp src/style.css bin/
 
-java --module-path "./School-Projet-Genie-Logiciel/javafx-sdk-17.0.15/lib" --add-modules javafx.controls,javafx.fxml -cp bin LabyrinthApp
+java --module-path "./linux-javafx-sdk-17.0.15/lib" --add-modules javafx.controls,javafx.fxml -cp bin LabyrinthApp
